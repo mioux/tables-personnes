f29859570827d9c7309f74affbe41d4585c7f967
Sylvain RUMEU <mioux-git@insertcoins.net>
2021-06-01 07:23:50 +0200

    Suppression de bootstrap qui ne fonctionne pas

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Source de la page : https://makitweb.com/datatables-ajax-pagination-with-search-and-sort-php/ -->
-    <!-- BOOTSTRAP -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <!-- /BOOTSTRAP -->
 
     <!-- JQUERY -->
       <link href='//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
@@ -17,7 +13,7 @@
       <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <!-- /JQUERY -->
 
-    <title>Assignation des prénoms depuis 1900 jusqu'en 2019</title>
+    <title>Assignation des prénoms depuis 2019</title>
 
     <!-- Activation DATATABLE -->
       <script language="ecmascript" type="text/ecmascript">
@@ -74,21 +70,10 @@
       <p>Le code département 20 est utilisé pour toute la corse, le code département 97 est utilisé pour la Guadeloupe, la Martinique, la Guyane et la Réunion</p>
     </div>
     <div>
-      <!-- Tabs navs -->
-      <ul class="nav nav-tabs mb-3" id="ex1" role="tablist">
-        <li class="nav-item" role="presentation">
-          <a class="nav-link active" id="ex1-tab-1" data-mbd-toggle="tab" href="#ex1-tabs-1" role="tab" aria-controls="ex1-tabs-1" aria-selected="true">National</a>
-        </li>
-        <li>
-          <a class="nav-link active" id="ex1-tab-2" data-mbd-toggle="tab" href="#ex1-tabs-2" role="tab" aria-controls="ex1-tabs-2" aria-selected="false">Départemental</a>
-        </li>
-      </ul>
-      <!-- /Tabs navs -->
 
-      <!-- Tabs content -->
-      <div class="tab-content" id="ex1-content">
-        <div class="tab-pane fade show active" id="ex1-tabs-1" role="tabpanel" aria-labelledby="ex1-tab-1">
-          <table id="national" class="display datatable">
+      <h2>National</h2>
+      <div>
+        <table id="national" class="display datatable">
             <thead>
               <tr>
                 <th>Sexe</th>
@@ -98,10 +83,11 @@
               </tr>
             </thead>
           </table>
-        </div>
-        
-        <div class="tab-pane fade show active" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
-          <table id="departement" class="display datatable">
+      </div>
+      <hr>
+      <h2>Départemental</h2>
+      <div>
+        <table id="departement" class="display datatable">
             <thead>
               <tr>
                 <th>Sexe</th>
@@ -112,10 +98,7 @@
               </tr>
             </thead>
           </table>
-        </div>
       </div>
     </div>
-    <!-- /Tabs content -->
   </body>
 </html> 
-  
