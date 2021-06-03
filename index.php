a7fc4ec937bf29fa5063fcd0532ccbb172dde666
Sylvain RUMEU <mioux-git@insertcoins.net>
2021-06-03 08:38:27 +0200

    HTML5, suppression d'erreurs + aide textuelle sur la recherche

--- index.php
+++ index.php
@@ -130,80 +130,81 @@
 
   <body>
     <div>
-      <p><h1>Assignation des prénoms depuis 1900 jusqu'en 2019</h1></p>
+      <h1>Assignation des prénoms depuis 1900 jusqu'en 2019</h1>
       <p>Conditions portant sur les prénoms retenus :</p>
-      <p>
-        <ol>
-          <li>Sur la période allant de 1900 à 1945, le prénom a été attribué au moins 20 fois à des personnes de sexe féminin et/ou au moins 20 fois à des personnes de sexe masculin</li>
-          <li>Sur la période allant de 1946 à 2019, le prénom a été attribué au moins 20 fois à des personnes de sexe féminin et/ou au moins 20 fois à des personnes de sexe masculin</li>
-          <li>Pour une année de naissance donnée, le prénom a été attribué au moins 3 fois à des personnes de sexe féminin ou de sexe masculin</li>
-        </ol>
-      </p>
+      <ol>
+        <li>Sur la période allant de 1900 à 1945, le prénom a été attribué au moins 20 fois à des personnes de sexe féminin et/ou au moins 20 fois à des personnes de sexe masculin</li>
+        <li>Sur la période allant de 1946 à 2019, le prénom a été attribué au moins 20 fois à des personnes de sexe féminin et/ou au moins 20 fois à des personnes de sexe masculin</li>
+        <li>Pour une année de naissance donnée, le prénom a été attribué au moins 3 fois à des personnes de sexe féminin ou de sexe masculin</li>
+      </ol>
       <p>Les effectifs des prénoms ne remplissant pas les conditions 1 et 2 sont regroupés (pour chaque sexe et chaque année de naissance) dans un enregistrement dont le champ prénom (PREUSUEL) prend la valeur «_PRENOMS_RARES_». Les effectifs des prénoms remplissant la condition 2 mais pas la condition 3 sont regroupés (pour chaque sexe et chaque prénom) dans un enregistrement dont le champ année de naissance (ANNAIS) est NULL.</p>
       <p>Le code département 20 est utilisé pour toute la corse, le code département 97 est utilisé pour la Guadeloupe, la Martinique, la Guyane et la Réunion</p>
+      <p>Attention : la recherche se fait désormais sur le terme exact. Toutefois, il est possible de mettre des jokers dans la recherche</p>
+      <ul>
+        <li>Le signe % correspond à *, soit 0 ou plus caractères. Exemple : <pre>Sylv%</pre> recherche les prénoms commençant par « Sylv » (Sylvain, Sylvette, ...)</li>
+        <li>Le signe _ correspond à ?. soit 1 caractère exactement.  Exemple : <pre>_ylvain</pre> recherche les prénoms commençant par une lettre quelconque, puis « ylvain » (Sylvain, Çylvain). La lettre est obligatoire. « Ylvain » ne sera pas trouvé, ni « Jean-
+        sylvain »</li>
+      </ul>
     </div>
-    <div>
 
-      <div class="tab" id="tab_national" onclick="showTab('national')">National</div>
-      <div class="tab" id="tab_departement" onclick="showTab('departement')">Départemental</div>
-      <div class="tab" id="tab_national_decenie" onclick="showTab('national_decenie')">National (par décénie)</div>
-      <div class="tab" id="tab_departement_decenie" onclick="showTab('departement_decenie')">Départemental (par décénie)</div>
+    <div class="tab" id="tab_national" onclick="showTab('national')">National</div>
+    <div class="tab" id="tab_departement" onclick="showTab('departement')">Départemental</div>
+    <div class="tab" id="tab_national_decenie" onclick="showTab('national_decenie')">National (par décénie)</div>
+    <div class="tab" id="tab_departement_decenie" onclick="showTab('departement_decenie')">Départemental (par décénie)</div>
 
-      <div id="div_national">
-        <table id="national" class="display datatable">
-            <thead>
-              <tr>
-                <th>Sexe</th>
-                <th>Prénom (preusuel)</th>
-                <th>Année de naissance (annais)</th>
-                <th>Nombre</th>
-              </tr>
-            </thead>
-          </table>
-      </div>
+    <div id="div_national">
+      <table id="national" class="display datatable">
+          <thead>
+            <tr>
+              <th>Sexe</th>
+              <th>Prénom (preusuel)</th>
+              <th>Année de naissance (annais)</th>
+              <th>Nombre</th>
+            </tr>
+          </thead>
+        </table>
+    </div>
 
-      <div id="div_departement">
-        <table id="departement" class="display datatable">
-            <thead>
-              <tr>
-                <th>Sexe</th>
-                <th>Prénom (preusuel)</th>
-                <th>Année de naissance (annais)</th>
-                <th>Département (dpt)</th>
-                <th>Nombre</th>
-              </tr>
-            </thead>
-          </table>
-      </div>
-      </div>
+    <div id="div_departement">
+      <table id="departement" class="display datatable">
+          <thead>
+            <tr>
+              <th>Sexe</th>
+              <th>Prénom (preusuel)</th>
+              <th>Année de naissance (annais)</th>
+              <th>Département (dpt)</th>
+              <th>Nombre</th>
+            </tr>
+          </thead>
+        </table>
+    </div>
+    </div>
 
-      <div id="div_national_decenie">
-        <table id="national_decenie" class="display datatable">
-            <thead>
-              <tr>
-                <th>Sexe</th>
-                <th>Prénom (preusuel)</th>
-                <th>Année de naissance (annais)</th>
-                <th>Nombre</th>
-              </tr>
-            </thead>
-          </table>
-      </div>
+    <div id="div_national_decenie">
+      <table id="national_decenie" class="display datatable">
+          <thead>
+            <tr>
+              <th>Sexe</th>
+              <th>Prénom (preusuel)</th>
+              <th>Année de naissance (annais)</th>
+              <th>Nombre</th>
+            </tr>
+          </thead>
+        </table>
+    </div>
 
-      <div id="div_departement_decenie">
-        <table id="departement_decenie" class="display datatable">
-            <thead>
-              <tr>
-                <th>Sexe</th>
-                <th>Prénom (preusuel)</th>
-                <th>Année de naissance (annais)</th>
-                <th>Département (dpt)</th>
-                <th>Nombre</th>
-              </tr>
-            </thead>
-          </table>
-      </div>
-
+    <div id="div_departement_decenie">
+      <table id="departement_decenie" class="display datatable">
+          <thead>
+            <tr>
+              <th>Sexe</th>
+              <th>Prénom (preusuel)</th>
+              <th>Année de naissance (annais)</th>
+              <th>Département (dpt)</th>
+              <th>Nombre</th>
+            </tr>
+          </thead>
+        </table>
     </div>
   </body>
 </html>
